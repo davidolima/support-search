dd43f381184ef27f929b2df9fcce43e5c6f24098
David <david@g15>
2024-06-14 20:00:20 -0300

    `generate_support_set` to `SupportSet.random_from_tensor`

--- utils.py
+++ utils.py
@@ -33,29 +33,3 @@ def get_distance_function(dist_func_str: str):
             return lambda x1,x2: -nn.functional.cosine_similarity(x1,x2,dim=1)
         case _:
             raise ValueError(f"Distance function `{dist_func_str}` is not supported.")
-
-def generate_support_set(
-        train_images: torch.Tensor,
-        train_labels: torch.Tensor,
-        n_way: int,
-        k_shot: int,
-        img_size: int,
-        img_channels: int,
-        device: Optional[str] = "cuda",
-):
-    data_tensor = torch.empty((n_way, k_shot, img_channels, img_size, img_size), device=device)
-    for i in range(n_way):
-        # Get all images of class `i`
-        cls_idxs = torch.where(train_labels == i)
-        class_imgs = train_images[cls_idxs]
-        print(class_imgs.shape)
-
-        # Select `k` random images of current class
-        k_idxs = torch.randperm(class_imgs.size(0))[:k_shot]
-        class_imgs = class_imgs[k_idxs]
-        print(class_imgs.shape)
-        class_imgs = class_imgs.view(k_shot,-1,img_size,img_size)
-        data_tensor[i] = class_imgs
-        print('---')
-
-    return data_tensor
