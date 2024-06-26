802d36e2bf77d612ed9f8c2ae99a8dccaa93c648
Destoper <marciojunior.gm@gmail.com>
2024-06-26 14:56:38 -0300

    feat: confusion matrix on test

--- siamese/test_that_works(Fashion_MNIST).py
+++ siamese/test_that_works(Fashion_MNIST).py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 from torchvision.transforms import transforms
 from torchvision.datasets import FashionMNIST
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 from siamese_fashion_MNIST_backbone_network import SiameseFashionMNISTBackboneNetwork
 from siamese_network import SiameseNetwork
 from torch.utils.data import DataLoader
@@ -61,6 +63,8 @@ with torch.no_grad():
         support_embeddings.append(embedding)
 
 correct, incorrect = 0, 0
+true_labels = []
+predicted_labels = []
 
 for idx, (x, label) in enumerate(data_loader):
     x_embedding = siamese_network.forward_once(x)
@@ -77,6 +81,28 @@ for idx, (x, label) in enumerate(data_loader):
         correct +=1
     else: 
         incorrect +=1
-
+    
+    true_labels.append(label.item())
+    predicted_labels.append(predicted_label)
 
 print(f"Accuracy: {correct / (correct + incorrect) * 100:.2f}%")
+
+# Calculate confusion matrix
+conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+# Plot confusion matrix with values
+plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.plasma)
+
+# Add annotations with values
+for i in range(len(fashion_MNIST_test_dataset.classes)):
+    for j in range(len(fashion_MNIST_test_dataset.classes)):
+        plt.text(j, i, str(conf_matrix[i, j]), horizontalalignment='center', verticalalignment='center')
+
+plt.title('Confusion Matrix')
+plt.colorbar()
+tick_marks = np.arange(len(fashion_MNIST_test_dataset.classes))
+plt.xticks(tick_marks, fashion_MNIST_test_dataset.classes, rotation=45)
+plt.yticks(tick_marks, fashion_MNIST_test_dataset.classes)
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.savefig('siamese/test_images/confusion_matrix.png')
